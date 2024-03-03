1590d8315fa9bf252a058da1ac5592cfb84a9a50
mohamed hassan <fed.mohamed.hassan@gmail.com>
2024-03-03 17:42:03 +0200

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { GlopalService } from './shared/services/glopal.service';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(private glopalService:GlopalService,private translateService:TranslateService){}
+  constructor(private glopalService:GlopalService,private translateService:TranslateService,private http:HttpClient){}
   ngOnInit(): void {
     this.glopalService.getNavbarAndFooterData()
     this.glopalService.getPages()
 
     this.lang()
+    this.http.get('https://openlibrary.org/subjects/Finance.json?details=false').subscribe(console.log)
     }
 
  lang() {
